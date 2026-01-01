8a906670f522691aa7cc179af5fa8b66d2205b51
IvanGamper <ivagamper1991@gmail.com>
2026-01-01 21:01:08 +0100

    f

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("testBtn");
+const output = document.getElementById("output");
+
+btn.addEventListener("click", () => {
+    output.textContent = "Backend kommt als Nächstes 🚀";
+});
